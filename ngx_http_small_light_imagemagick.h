65b4f2b663c83e91f0802825c309fe6d938baf46
cubicdaiya <cubicdaiya@gmail.com>
2013-01-13 09:09:42 +0900

    update copyright year

--- ngx_http_small_light_imagemagick.h
+++ ngx_http_small_light_imagemagick.h
@@ -1,5 +1,5 @@
 /**
-   Copyright (c) 2012 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+   Copyright (c) 2013 Tatsuhiko Kubo <cubicdaiya@gmail.com>
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
