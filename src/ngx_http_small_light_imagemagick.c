299a556d74d89c6c408888d2590d7a649fdabf59
chiastolite <chiastolite.1980@gmail.com>
2016-03-25 23:57:15 +0900

    Fix indentation to 4-space

--- src/ngx_http_small_light_imagemagick.c
+++ src/ngx_http_small_light_imagemagick.c
@@ -240,18 +240,18 @@ ngx_int_t ngx_http_small_light_imagemagick_process(ngx_http_request_t *r, ngx_ht
         /* copy original image profiles to the new canvas. */
         origin_image_profiles = MagickGetImageProfiles(ictx->wand, "*", &profile_count);
         if (origin_image_profiles != (char **) NULL) {
-          for (i = 0; i < profile_count; i++) {
-            profile = MagickGetImageProfile(ictx->wand, origin_image_profiles[i], &profile_len);
+            for (i = 0; i < profile_count; i++) {
+                profile = MagickGetImageProfile(ictx->wand, origin_image_profiles[i], &profile_len);
 
-            status = MagickSetImageProfile(canvas_wand, origin_image_profiles[i], profile, profile_len);
+                status = MagickSetImageProfile(canvas_wand, origin_image_profiles[i], profile, profile_len);
 
-            if (status == MagickFalse) {
-              r->err_status = NGX_HTTP_INTERNAL_SERVER_ERROR;
-              DestroyMagickWand(canvas_wand);
-              DestroyString(of_orig);
-              return NGX_ERROR;
+                if (status == MagickFalse) {
+                    r->err_status = NGX_HTTP_INTERNAL_SERVER_ERROR;
+                    DestroyMagickWand(canvas_wand);
+                    DestroyString(of_orig);
+                    return NGX_ERROR;
+                }
             }
-          }
         }
 
         DestroyMagickWand(ictx->wand);
